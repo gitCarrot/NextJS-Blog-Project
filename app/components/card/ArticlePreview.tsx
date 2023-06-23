90e5841ca1f22dea6c7989dcf8ed14364969ce4f
gitCarrot <jeonhg0@gmail.com>
2023-06-23 04:54:08 -1000

    extra design

--- app/components/card/ArticlePreview.tsx
+++ app/components/card/ArticlePreview.tsx
@@ -28,7 +28,7 @@ const ArticlePreview: React.FC<Props> = ({
         <div className={`mt-8 h-[19rem] md:h-[25rem] lg:h-[25rem] rounded-3xl overflow-hidden ${dark ? "dark" : ""}`}
       style={{ background: `${bgColor}` }}>
           <div
-            className={`h-full w-full object-scale-down px-10 py-6 duration-[500ms] hover:scale-105 bg-cover bg-no-repeat bg-center transition-all ease-in-out`}
+            className={`h-full w-full object-scale-down sm:bg-bottom-center px-10 py-6 duration-[500ms] hover:scale-105 bg-cover bg-no-repeat bg-center transition-all ease-in-out`}
             style={{ backgroundImage: `url('${imageUrl}')`}}
           >
             <div className="flex justify-between">
