1fb91d0cb5b2014559b1b3a97b1c4181c4040eb8
gitCarrot <jeonhg0@gmail.com>
2023-06-22 23:21:06 -1000

    test

--- app/articles/[category]/page.tsx
+++ app/articles/[category]/page.tsx
@@ -1,21 +1,11 @@
 import ArticlePreview from '@/app/components/card/ArticlePreview';
 import getPostMetadata from '@/app/actions/getPostMetadata';
 
-const page = ( props: any ) => {
-  const category = props.params.category;
-  const postMetadata = getPostMetadata(category);
-  const image = `/${category}.png`;
-  const previews = postMetadata.map((post) => (
-    <ArticlePreview 
-    key={post.slug}
-    PostMetaData={post}
-    imageUrl={image}
-    category={category}
-    />
-  ))
+import React from 'react'
+
+const page = () => {
   return (
-    <div className="mt-8">{previews}</div>
-    
+    <div>page</div>
   )
 }
 
